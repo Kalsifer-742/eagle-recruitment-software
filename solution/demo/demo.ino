84056aab7c299d75adb61c8f82112100b854d83d
Kalsifer <kalsifer742@gmail.com>
2023-09-25 23:06:48 +0200

    added missing features and corrected sensor data collection

--- solution/demo/demo.ino
+++ solution/demo/demo.ino
@@ -39,7 +39,7 @@ typedef struct {
     Action action;
 } Transition;
 
-// Transition table: [currentState][event]
+// transition_table[current_state][event]
 Transition transition_table[3][4] = 
 {
   //RUNNING_STATE
@@ -54,20 +54,26 @@ void handle_danger_to_running_state_transition(void)
 {
   digitalWrite(undervoltage_led_pin, LOW);
   digitalWrite(overvoltage_led_pin, LOW);
+  Serial.println("leds_status: undervoltage_led is OFF");
+  Serial.println("leds_status: overvoltage_led is OFF");
   return;
 }
 
 void handle_undervoltage_led(void)
 {
   digitalWrite(overvoltage_led_pin, LOW);
+  Serial.println("leds_status: overvoltage_led is OFF");
   digitalWrite(undervoltage_led_pin, HIGH);
+  Serial.println("leds_status: undervoltage_led is ON");
   return;
 }
 
 void handle_overvoltage_led(void)
 {
   digitalWrite(undervoltage_led_pin, LOW);
+  Serial.println("leds_status: undervoltage_led is OFF");
   digitalWrite(overvoltage_led_pin, HIGH);
+  Serial.println("leds_status: overvoltage_led is ON");
   return;
 }
 
@@ -88,7 +94,7 @@ void handle_waiting_to_running_state_transition()
 void transitions_handler(Event event)
 {
   Transition transition = transition_table[current_state.state][event];
-  //handle this also with a function pointer ?
+  //handle also this with a function pointer ?
   switch(transition.action)
   {
     case POWER_OFF_LEDS:
@@ -132,9 +138,10 @@ void running_state(void)
   //timers running
 }
 
+//if I could create multiple states it would be easy to identify which LED is on
 void danger_state(void)
 {
-  //nothing to do
+  
 }
 
 void waiting_state(void)
@@ -143,47 +150,89 @@ void waiting_state(void)
   delay(500);
 }
 
+void ms_to_hhmmssms(unsigned long& ms, int& hours, int& minutes, int& seconds)
+{
+  seconds = ms / 1000;
+  ms = ms % 1000;
+
+  minutes = seconds / 60;
+  seconds = seconds % 60;
+
+  hours = minutes / 60;
+  minutes = minutes % 60;
+}
+
+//maybe create a dedicated function for printing
+void print_timestamp()
+{
+  unsigned long ms = millis();
+  int h, m, s;
+  ms_to_hhmmssms(ms, h, m, s);
+  Serial.print(h);
+  Serial.print(":");
+  Serial.print(m);
+  Serial.print(":");
+  Serial.print(s);
+  Serial.print(":");
+  Serial.print(ms);
+}
+
+//The only impossible value is below 0 lux but it can't happen with this approach
 void light_sensor_timer_interrupt_handler(void)
 {
-  //add timestamps ?
   int analog_value = analogRead(light_sensor_pin);
-  int voltage = analog_value * (5.0 / 1023.0);
-  int lux = exp(-0.5852985121078413 * voltage + 5.546777762224811);
-  Serial.print("Brightness: ");
+  float voltage = analog_value * (5.0 / 1023.0);
+  //int lux = exp(-0.5852985121078413 * voltage + 5.546777762224811);
+  int lux = 123.8957 + (65237400000 - 123.8957) / (1 + pow((voltage / 0.00003897886), 1.778511)); //more accurate in the higher range
+  print_timestamp();
+  Serial.print(" brightness: ~ ");
+
+  /*int lux = 0;
+  if (voltage > 4.30) {
+    lux = 50;
+    Serial.print("<");
+  } else if (voltage > 0.90) {
+    lux = 500;
+  } else {
+    lux = 1200;
+    Serial.print(">");
+  }
+  */
+  
   Serial.print(lux);
-  Serial.println(" [Lux]");
+  Serial.println("Lux");
+  //Serial.println(voltage);
 }
 
 void voltage_timer_interrupt_handler(void)
 {
   int sensor_value = analogRead(voltage_pin);
   float voltage = sensor_value * (5.0 / 1023.0);
-  Serial.print("Voltage: ");
+  print_timestamp();
+  Serial.print(" voltage: ");
   Serial.print(voltage);
-  Serial.println(" [V]");
-
+  Serial.println("V");
   if (voltage < 1.8)
   {
-    Event e = UNDERVOLTAGE; //can i skip this ?
-    transitions_handler(e);
+    //Event e = UNDERVOLTAGE;
+    transitions_handler(UNDERVOLTAGE);
   }
   else if (voltage > 2.7) 
   {
-    Event e = OVERVOLTAGE;
-    transitions_handler(e);
+    //Event e = OVERVOLTAGE;
+    transitions_handler(OVERVOLTAGE);
   }
   else 
   {
-    Event e = VOLTAGE_IN_RANGE;
-    transitions_handler(e);
+    //Event e = VOLTAGE_IN_RANGE;
+    transitions_handler(VOLTAGE_IN_RANGE);
   }
 }
 
-//volatile ?
 void button_interrupt_handler(void)
 {
-  Event e = EMERGENCY_BUTTON_PRESSED;
-  transitions_handler(e);
+  //Event e = EMERGENCY_BUTTON_PRESSED;
+  transitions_handler(EMERGENCY_BUTTON_PRESSED);
 }
 
 void setup() 
@@ -194,10 +243,9 @@ void setup()
   pinMode(emergency_button, INPUT);
   attachInterrupt(digitalPinToInterrupt(emergency_button), button_interrupt_handler, HIGH);
 
-  //change timings to correct ones
-  light_sensor_check_timer->setOverflow(1000000, MICROSEC_FORMAT); 
+  light_sensor_check_timer->setOverflow(1000000, MICROSEC_FORMAT); //200000
   light_sensor_check_timer->attachInterrupt(light_sensor_timer_interrupt_handler);
-  voltage_check_timer->setOverflow(500000, MICROSEC_FORMAT);
+  voltage_check_timer->setOverflow(1000000, MICROSEC_FORMAT); //350000
   voltage_check_timer->attachInterrupt(voltage_timer_interrupt_handler);
   light_sensor_check_timer->resume();
   voltage_check_timer->resume();
