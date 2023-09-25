dc9e1d4f7491af3541486039fbd778167ca94bd2
Kalsifer <kalsifer742@gmail.com>
2023-09-25 15:52:10 +0200

    implemented all feature requested. TODO: refactor code into a state machine

--- solution/demo/demo.ino
+++ solution/demo/demo.ino
@@ -1,39 +1,86 @@
 const int light_sensor_pin = A5;
-unsigned long start_time; 
-unsigned long current_time;
-const unsigned long light_sensor_period = 200;
-const unsigned long voltage_period = 350;
+const int voltage_pin = A0;
+const int undervoltage_led_pin = D2;
+const int overvoltage_led_pin = D4;
+const int emergency_button = PC13;
+bool waiting = false;
 
-void print_reading(int value)
+HardwareTimer *light_sensor_timer = new HardwareTimer(TIM6);
+HardwareTimer *voltage_check_timer = new HardwareTimer(TIM7);
+
+enum States {
+  danger_state,
+  waiting_state,
+  running_state
+};
+
+//volatile ?
+void wait_state(void)
 {
-  Serial.print("Analog reading: ");
-  Serial.print(value);   // the raw analog reading
-
-  // We'll have a few threshholds, qualitatively determined
-  if (value < 10) {
-    Serial.println(" - Dark");
-  } else if (value < 200) {
-    Serial.println(" - Dim");
-  } else if (value < 500) {
-    Serial.println(" - Light");
-  } else if (value < 800) {
-    Serial.println(" - Bright");
-  } else {
-    Serial.println(" - Very bright");
-  }
+  waiting = !waiting;
 }
 
-void setup() {
-  Serial.begin(9600);
-  start_time = millis();
+void read_light_sensor(void)
+{
+  int analog_value = analogRead(light_sensor_pin);
+  int voltage = analog_value * (5.0 / 1023.0);
+  int lux = exp(-0.5852985121078413 * voltage + 5.546777762224811);
+  Serial.print("Brightness: ");
+  Serial.print(lux);
+  Serial.println(" lx"); //lux
 }
 
-void loop() {
-    current_time = millis();
-  if (current_time - start_time >= light_sensor_period)  //test whether the period has elapsed
+void read_voltage(void)
+{
+  int sensor_value = analogRead(voltage_pin);
+  float voltage = sensor_value * (5.0 / 1023.0);
+  if (voltage < 1.8) 
   {
-    int analog_value = analogRead(light_sensor_pin); //value between 0 and 1023
-    print_reading(analog_value);
-    start_time = current_time;
+    digitalWrite(undervoltage_led_pin, HIGH);
+  }
+  else if (voltage > 2.7) 
+  {
+    digitalWrite(overvoltage_led_pin, HIGH);
+  }
+  else 
+  {
+    digitalWrite(undervoltage_led_pin, LOW);
+    digitalWrite(overvoltage_led_pin, LOW);
+  }
+  Serial.print("Voltage: ");
+  Serial.println(voltage);
+}
+
+void setup() 
+{
+  Serial.begin(9600);
+  pinMode(undervoltage_led_pin, OUTPUT);
+  pinMode(overvoltage_led_pin, OUTPUT);
+  pinMode(emergency_button, INPUT);
+  attachInterrupt(digitalPinToInterrupt(emergency_button), wait_state, HIGH);
+
+  light_sensor_timer->setOverflow(1000000, MICROSEC_FORMAT); 
+  light_sensor_timer->attachInterrupt(read_light_sensor);
+  voltage_check_timer->setOverflow(500000, MICROSEC_FORMAT);
+  voltage_check_timer->attachInterrupt(read_voltage);
+
+  light_sensor_timer->resume();
+  voltage_check_timer->resume();
+}
+
+void loop() 
+{
+  if (waiting) {
+    light_sensor_timer->pause();
+    voltage_check_timer->pause();
+    while (true) {
+      if (!waiting) {
+        break;
+      }
+      Serial.println("Board in waiting state - please press the emergency button");
+      delay(500);
+    }
+    light_sensor_timer->resume();
+    voltage_check_timer->resume();
   }
 }
